01ba5fbd203a39000b51ec533ffb8cc2744697fc
ssammarshall <ssammarshall@gmail.com>
2026-04-22 12:12:59 -0700

    NY IFVG liquidity sweep setup init commit

---
+++ app/strategies/setups/ny_ifvg_liquidity_sweep_setup.py
@@ -0,0 +1,9 @@
+from base_setup import BaseSetup
+
+class NYIFVGLiquiditySweepSetup(BaseSetup):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def is_valid(self) -> bool:
+        # Implement the validation logic for the NYIFVGLiquiditySweepSetup
+        pass
