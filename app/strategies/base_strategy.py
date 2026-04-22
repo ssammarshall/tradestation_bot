7d94e68eb6ed03a2d2ae29b6f9ea885d3d2e23ed
ssammarshall <ssammarshall@gmail.com>
2026-04-22 10:49:16 -0700

    Strategy __init__ func requiring setup/entry combo. Setup/entry checked for validity during strategy evaluate

--- app/strategies/base_strategy.py
+++ app/strategies/base_strategy.py
@@ -1,6 +1,30 @@
-from typing import Protocol
+from time import time
 
-class BaseStrategy(Protocol):
+from app.strategies.entries.base_entry import BaseEntry
+from app.strategies.setups.base_setup import BaseSetup
+
+
+class BaseStrategy:
+    def __init__(
+        self,
+        symbol: str,
+        setup: BaseSetup,
+        entry: BaseEntry,
+        trade_window_start: time,
+        trade_window_end: time,
+        max_num_of_trades: int,
+    ) -> None:
+        self.symbol = symbol
+        self.setup = setup
+        self.entry = entry
+        self.trade_window_start = trade_window_start
+        self.trade_window_end = trade_window_end
+        self.max_num_of_trades = max_num_of_trades
+        self._setup_confirmed: bool = False
+
     def evaluate(self) -> None:
-        """Evaluate the strategy."""
-        ...
+        if not self._setup_confirmed:
+            if self.setup.is_valid():
+                self._setup_confirmed = True
+        else:
+            self.entry.is_valid()
