9faaaf0c04c89824912736472cecd833601ccf0b
ssammarshall <ssammarshall@gmail.com>
2026-05-01 07:48:40 -0700

    History params for setup startup

--- app/strategies/setups/base_setup.py
+++ app/strategies/setups/base_setup.py
@@ -1,11 +1,14 @@
 from abc import ABC, abstractmethod
 
-from app.schemas.bars import Bar
+from app.schemas.bars import Bar, BarHistoryParams
 
 
 class BaseSetup(ABC):
     def __init__(self) -> None:
         pass
+
+    @abstractmethod
+    def history_params(self, symbol: str) -> BarHistoryParams: ...
 
     @abstractmethod
     def startup(self, bars: list[Bar]) -> None: ...
